5540d38687ff9692414903930aca0a10e4eeb127
Stefanopellegrinoo <stefanopellegrino490@gmail.com>
2026-04-17 19:26:20 -0300

    Front

--- lib/axios.ts
+++ lib/axios.ts
@@ -8,7 +8,7 @@ import axios, { type AxiosError, type AxiosRequestConfig, type AxiosResponse } f
 //   "http://localhost:3003/api"
 
 export const baseURL =
-  "https://party.fratellipastas.com"
+  "http://100.81.177.86:3000"
 // como no tengo url propia hay que tirar por consola
 // cloudflared tunnel --url http://localhost:3000
 // cloudflared tunnel --url http://localhost:3000 --loglevel debug
